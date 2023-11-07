c24d394098259c7561c5c24da1f36385eaa8526d
KC-Nick <wilson.kasinichole@gmail.com>
2023-11-06 21:01:32 -0500

    update schema

--- db/schema.sql
+++ db/schema.sql
@@ -5,21 +5,29 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY,
+    id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id),
+    REFERENCES department(id),
+    PRIMARY KEY (id),
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    FOREIGN KEY (roles_id)
+    REFERENCES roles(id),
+    ON DELETE SET NULL
 );
